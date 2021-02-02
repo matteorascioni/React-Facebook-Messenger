3d33b3e1bc064c3a33f15f2d25c220325df0b8de
Matteo <Rascioni>
2021-02-02 16:56:18 +0100

    Start UI

--- facebook-messenger/src/App.js
+++ facebook-messenger/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Button } from '@material-ui/core';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
 
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // all the logic to send a message
+    event.preventDefault();
+    setMessages([messages, input]);
+    setInput(''); 
+  }
+
+  return(
+      <div className="App">
+          <form>
+          <input value={ input } onChange={event => setInput(event.target.value)} />
+          <Button 
+            variant="contained" 
+            color="primary" 
+            disabled={!input} 
+            type="submit" 
+            onClick={ sendMessage }
+          >
+              Send message
+          </Button>
+          </form>
+      
+          {
+          messages.map(message => 
+          <p>{ message }</p>
+          )
+          }
     </div>
-  );
+  )
 }
+
 
 export default App;
