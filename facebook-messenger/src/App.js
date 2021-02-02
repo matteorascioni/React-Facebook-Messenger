b6ea6a5f127533ad16bab7c2c33bbab24e879e06
Matteo <Rascioni>
2021-02-02 17:57:55 +0100

    UI 30%

--- facebook-messenger/src/App.js
+++ facebook-messenger/src/App.js
@@ -1,41 +1,55 @@
-import React, {useState} from 'react';
-import { Button } from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import Message from './Message';
+
 import './App.css';
 
 const App = () => {
   const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    {userName: 'Matteo', text: 'Hello'},
+    {userName: 'Livia', text: 'Hi'},
+  ]);
+  const [userName, setUserName] = useState('');
 
-  console.log(input);
-  console.log(messages);
+  useEffect(() => {
+    setUserName( prompt( 'Please enter your name' ) );
+  }, [])
 
   const sendMessage = (event) => {
     // all the logic to send a message
     event.preventDefault();
-    setMessages([messages, input]);
+    setMessages([
+      ...messages, { userName: userName, text: input },
+    ]);
     setInput(''); 
   }
 
   return(
       <div className="App">
-          <form>
-          <input value={ input } onChange={event => setInput(event.target.value)} />
-          <Button 
-            variant="contained" 
-            color="primary" 
-            disabled={!input} 
-            type="submit" 
-            onClick={ sendMessage }
-          >
-              Send message
-          </Button>
-          </form>
-      
-          {
-          messages.map(message => 
-          <p>{ message }</p>
-          )
-          }
+        <h1>Welcome {userName}</h1>
+
+        <form>
+          <FormControl>
+              <InputLabel>Enter a message...</InputLabel>
+              <Input value={ input } onChange={event => setInput(event.target.value)} />
+              <Button 
+              variant="contained" 
+              color="primary" 
+              disabled={!input} 
+              type="submit" 
+              onClick={ sendMessage }
+              >
+                  Send message
+              </Button>
+            </FormControl>
+        </form>
+     
+        {
+          messages.map(message => (
+            <Message userName={ message.userName } text={ message.text }/>
+          ))
+        }
     </div>
   )
 }
---
+++ facebook-messenger/src/Message.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Message = (props) => {
+    return (
+        <div>
+            <h2>{ props.userName} : { props.text }</h2>
+        </div>
+    )
+}
+
+export default Message;
+
